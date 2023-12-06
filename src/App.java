c85597e009f831220ffc2d6ed072b037c5b88434
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2023-12-06 09:01:20 -0300

    Criação dos submenus para Cliente e Imovel

--- src/App.java
+++ src/App.java
@@ -18,11 +18,13 @@ public class App {
 
             switch (op){
                 case 1:
-                    System.out.println("Em construcao");
+                    ler.nextLine();
+                    subMenu(1);
                 break;
 
                 case 2:
-                    System.out.println("Em construcao");
+                    ler.nextLine();
+                    subMenu(2);
                 break;
 
                 case 3:
@@ -45,6 +47,53 @@ public class App {
             }
 
 
+        }while(op != 0);
+    }
+
+    public static void subMenu(int gestão){
+        Scanner ler = new Scanner(System.in);
+        int op;
+        do{
+            if(gestão == 1){
+                System.out.println(">>> GESTAO DE CLIENTES <<<");
+            }else{
+                System.out.println(">>> GESTAO DE IMOVEIS <<<");
+            }
+            System.out.println("[1] INCLUIR");
+            System.out.println("[2] CONSULTAR");
+            System.out.println("[3] LISTAR");
+            System.out.println("[4] EXCLUIR");
+            System.out.println("[5] ALTERAR");
+            System.out.println("[0] SAIR");
+            op = ler.nextInt();
+
+            switch (op){
+                case 1:
+
+                break;
+
+                case 2:
+
+                break;
+
+                case 3:
+                    System.out.println("Em construcao");
+                break;
+
+                case 4:
+                    System.out.println("Em construcao");
+                break;
+
+                case 5:
+                    System.out.println("Em construcao");
+                break;
+
+                case 0:
+                break;
+
+                default:
+                    System.out.println("Ops... Opcao invalida!");
+            }
         }while(op != 0);
     }
 }
