1c98b55e46f59d2463a9867c83a9e1b934e95726
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2023-12-05 22:42:18 -0300

    Implementação parcial das classes Cliente e Imovel

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    //Nessa classe que vai ficar toda a lógica do negocio
+    // Ela contém o método Main() onde o programa começa a ser executado
+    public static void main(String[] args) {
+        int op;
+        do {
+            System.out.println(">>> MENU PRINCIPAL <<<");
+
+        }while(op != 0);
+    }
+}
