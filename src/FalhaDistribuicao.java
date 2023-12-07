320f47970252c3538405a58167c7cd7240acfb46
IanRobertLuzPinheiro <149024071+IanRobertLuzPinheiro@users.noreply.github.com>
2023-12-07 09:59:28 -0300

    Update FalhaDistribuicao.java

--- src/FalhaDistribuicao.java
+++ src/FalhaDistribuicao.java
@@ -1,2 +1,43 @@
-public class FalhaDistribuicao {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class FalhaDistribuicao extends Falha {
+  private ArrayList<Reparo> reparos;
+
+    public FalhaDistribuicao(String descricao, Date previsao) {
+        super(descricao, previsao);
+        reparos = new ArrayList<>();
+    }
+
+    public void adicionarReparo() throws ParseException{
+        String descricao;
+        Date previsao;
+        Scanner sc = new Scanner(System.in);
+        try{
+            System.out.println("Descreva a ação para corrigir o problema:");
+            descricao = sc.nextLine();
+            System.out.println("Informe a previsão de conclusão do reparo no formato: dd/mm/aaaa");
+            SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+            previsao = formato.parse(sc.nextLine());
+            Reparo reparo = new Reparo(descricao, previsao);
+            this.reparos.add(reparo);
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public void listarReparo(){
+        for(Reparo reparo : this.reparos){
+            if(!reparo.getResolvido()){
+                System.out.println("Descrição: " + reparo.getDescricao());
+                System.out.println("Início: " + reparo.getDataInicio());
+                System.out.println("Previsão: " + reparo.getPrevisao());
+            }
+        }
+    }
+
 }
