9da536403a65411c1675c39e309b23aaa52e6332
IanRobertLuzPinheiro <149024071+IanRobertLuzPinheiro@users.noreply.github.com>
2023-12-07 11:45:13 -0300

    Update FalhaGeracao.java

--- src/FalhaGeracao.java
+++ src/FalhaGeracao.java
@@ -1,2 +1,7 @@
-public class FalhaGeracao {
+import java.util.Date;
+
+public class FalhaGeracao extends Falha {
+   public FalhaGeracao(String descricao, Date previsao) {
+        super(descricao, previsao);
+    }
 }
